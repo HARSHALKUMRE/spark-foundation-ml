d34ebc3aea2304bceb63b23dc2bec4b9aec9ccc2
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-03-03 12:15:42 +0530

    spark ml foundation

--- decisiontreemlspark.py
+++ decisiontreemlspark.py
@@ -22,3 +22,54 @@ from pyspark.sql.functions import col
 new_dataset = dataset.select(*(col(c).cast("float").alias(c) for c in dataset.columns))
 
 new_dataset.printSchema()
+
+from pyspark.sql.functions import col, count, isnan, when
+
+new_dataset.select([count(when(col(c).isNull(), c)).alias (c) for c in new_dataset.columns]).show()
+
+cols = new_dataset.columns
+
+cols.remove("quality")
+
+assembler = VectorAssembler(inputCols=cols, outputCol="features")
+
+data = assembler.transform(new_dataset)
+
+data = data.select("features", "quality")
+
+data.show()
+
+from pyspark.ml.feature import StringIndexer
+
+stringIndexer = StringIndexer(inputCol="quality", outputCol="quality_index")
+
+data_Indexed = stringIndexer.fit(data).transform(data)
+
+data_Indexed.show()
+
+# split the data
+(train, test) = data_Indexed.randomSplit([0.7, 0.3])
+
+# module building
+dt = DecisionTreeClassifier(labelCol="quality_index", featuresCol="features")
+model = dt.fit(train)
+
+# for prediction
+prediction = model.transform(test)
+
+# for showcase the prediction
+prediction.show()
+
+# Evaluator
+evaluator = MulticlassClassificationEvaluator(labelCol="quality_index", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(prediction)
+print("Accuracy:- ",accuracy)
+
+# Saving the model
+model.save("decisiontreeclassifier.pkl")
+
+# load the model
+model.load("")
+
+# stop spark
+# spark.stop()
